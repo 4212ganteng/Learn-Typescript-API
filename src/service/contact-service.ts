493cef504ff400ca8b5a6b5fd41c153d1cb1883b
4212ganteng <azizmuslim051@gmail.com>
2024-10-25 16:42:03 +0700

    refactor contact service, create service update contact, create controller update contact

--- src/service/contact-service.ts
+++ src/service/contact-service.ts
@@ -1,15 +1,36 @@
-import { User } from '@prisma/client';
+import { Contact, User } from '@prisma/client';
 import {
   ContactResponse,
   CreateContactRequest,
   toContactResponse,
+  updateContactRequest,
 } from '../model/cintact-model';
 import { ContactValidation } from '../validation/contact-validation';
 import { Validation } from '../validation/validation';
 import { prismaClient } from '../application/database';
 import { ResponseError } from '../error/response-error';
+import { UpdateUserRequest } from '../model/user-model';
 
 export class ContactService {
+  // function Contact user isExists
+  static async checkContactMustExists(
+    username: string,
+    contactId: number
+  ): Promise<Contact> {
+    const contact = await prismaClient.contact.findUnique({
+      where: {
+        id: contactId,
+        username: username,
+      },
+    });
+
+    if (!contact) {
+      throw new ResponseError(404, 'Contact not found');
+    }
+
+    return contact;
+  }
+
   static async create(
     user: User,
     request: CreateContactRequest
@@ -33,16 +54,27 @@ export class ContactService {
   }
 
   static async get(user: User, id: number): Promise<ContactResponse> {
-    const contact = await prismaClient.contact.findUnique({
-      where: {
-        id: id,
-        username: user.name,
-      },
-    });
+    const contact = await this.checkContactMustExists(user.username, id);
+    return toContactResponse(contact);
+  }
 
-    if (!contact) {
-      throw new ResponseError(404, 'Contact not found');
-    }
+  static async update(
+    user: User,
+    request: updateContactRequest
+  ): Promise<ContactResponse> {
+    const updateRequest = Validation.validate(
+      ContactValidation.UPDATE,
+      request
+    );
+    await this.checkContactMustExists(user.username, updateRequest.id);
+
+    const contact = await prismaClient.contact.update({
+      where: {
+        id: updateRequest.id,
+        username: user.username,
+      },
+      data: updateRequest,
+    });
 
     return toContactResponse(contact);
   }
